1fa5eb8cef75cba481dab1acdab5da97f32a2e03
Maxim Revyakin <mrevyakin@gmail.com>
2017-04-08 23:13:59 +0300

    group-list moved to table markup; sorting added

--- app/public/group-management/group-list/group-list.component.js
+++ app/public/group-management/group-list/group-list.component.js
@@ -4,5 +4,23 @@ angular
 		templateUrl: 'group-management/group-list/group-list.template.html',
 		controller: function groupListController(Group) {
 			this.groups = Group.query();
+
+			this.sortState = {
+				column: 'name',
+				order: undefined
+			};
+
+			this.sort = function(sortBy) {
+				if (sortBy !== 'name') {
+					return;
+				}
+				this.sortState.order =
+					this.sortState.order === 'inc' ? 'dec' : 'inc';
+
+				this.groups.sort(function(a, b) {
+					var order = this.sortState.order === 'inc' ? 1 : -1;
+					return order * a.groupName.localeCompare(b.groupName);
+				}.bind(this));
+			};
 		}
 	});
