e722b6b562a539e54e873ca0d0a187696602876f
Maxim Revyakin <mrevyakin@gmail.com>
2017-07-25 13:55:26 +0300

    question set use count introduced

--- app/public/question-set-management/question-set-list/question-set-list.component.js
+++ app/public/question-set-management/question-set-list/question-set-list.component.js
@@ -2,7 +2,17 @@ angular
 	.module('questionSetList')
 	.component('questionSetList', {
 		templateUrl: 'question-set-management/question-set-list/question-set-list.template.html',
-		controller: function questionSetListController(QuestionSet) {
+		controller: function questionSetListController(QuestionSet, Testing) {
 			this.questionSets = QuestionSet.query();
+
+			this.useCount = {};
+			Testing.query(function(testings) {
+				testings.forEach(function(t) {
+					if (!this.useCount[t.idQuestionSet]) {
+						this.useCount[t.idQuestionSet] = 0;
+					}
+					this.useCount[t.idQuestionSet] += 1;
+				}, this);
+			}.bind(this));
 		}
 	});
