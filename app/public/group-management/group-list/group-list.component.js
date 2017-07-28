994dd63f21c572720ec99f42650f68070ddb4fa5
Maxim Revyakin <mrevyakin@gmail.com>
2017-07-28 19:19:48 +0300

    filter groups by a testee name

--- app/public/group-management/group-list/group-list.component.js
+++ app/public/group-management/group-list/group-list.component.js
@@ -26,5 +26,45 @@ angular
 					return order * a.groupName.localeCompare(b.groupName);
 				}.bind(this));
 			};
+
+			this.studentsFilter = function(actual, _expected) {
+				var queryParts, nameParts, i, j, found,
+					expected = _expected.toLowerCase();
+				if (typeof actual !== 'object' || actual.students === undefined) {
+					return false;
+				}
+
+				queryParts = expected.split(' ');
+				return actual.students.some(function(s) {
+					nameParts = ['firstName', 'lastName', 'patronymic'].map(function(field) {
+						return s[field].toLowerCase();
+					});
+
+					if (nameParts.some(function(name) {
+						return name.indexOf(expected) !== -1;
+					})) {
+						return true;
+					}
+
+					if (queryParts.length > 3) {
+						return false;
+					}
+
+					for (i = 0; i < queryParts.length; ++i) {
+						found = false;
+						for (j = 0; j < nameParts.length; ++j) {
+							if (nameParts[j].indexOf(queryParts[i]) !== -1) {
+								found = true;
+								nameParts.splice(j, 1);
+								break;
+							}
+						}
+						if (!found) {
+							return false;
+						}
+					}
+					return true;
+				});
+			};
 		}
 	});
