d868ef21707881f39c26dea9d436a9b44786861e
Maxim Revyakin <mrevyakin@gmail.com>
2017-08-01 15:21:08 +0300

    db initialization introduced

--- app/db/db-create.js
+++ app/db/db-create.js
@@ -1,90 +1,70 @@
-let
-	execute = require('./lib/promise-executer'),
-	mongoose = require('mongoose'),
+#!/usr/bin/env node
 
-	connectionOpen,
-	databaseDrop,
-	modelsRequire,
-	userCreate,
-	// TODO: remove userTest
-	userTest,
-	factorSetCreate;
+let execute = require('../lib/promise-executer'),
+	mongoose = require('mongoose'),
+	dbBackup = require('../lib/db-backup-create'),
+
+	connectionOpen = function() {
+		return new Promise(function(resolve, reject) {
+			mongoose.connection.on('open', resolve);
+			mongoose.connection.on('error', reject);
+		});
+	},
+
+	databaseDrop = function() {
+		return new Promise(function(resolve, reject) {
+			mongoose.connection.dropDatabase(err => {
+				if (err) {
+					reject(err);
+					return;
+				}
+				resolve();
+			});
+		});
+	},
+
+	modelsRequire = function() {
+		require('./users-schema');
+		require('./factor-sets-schema');
+
+		return Promise.all(
+			Object.keys(mongoose.models).map(model => {
+				return mongoose.models[model].ensureIndexes();
+			})
+		);
+	},
+
+	userCreate = function() {
+		let Users = require('./users-schema'),
+			user = new Users({
+				login: 'Admin',
+				password: 'admin'
+			});
+
+		return user.save();
+	},
+
+	factorSetCreate = function() {
+		// TODO: Cattell factor set JSON data
+	};
 
 mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/studentsTesting');
 
 execute(function* () {
 	try {
 		yield connectionOpen();
+		//yield dbBackup();
 		yield databaseDrop();
 		yield modelsRequire();
 		yield userCreate();
 
-		console.log(
-			yield userTest()
-		);
-
-		yield factorSetCreate();
+		//yield factorSetCreate();
 
 	} catch (err) {
 		if (err) {
 			console.error(err);
 		}
-		db.close();
 	}
+	mongoose.connection.close();
 }());
-
-connectionOpen = function() {
-	return new Promise(resolve, reject => {
-		db.connection.on('open', resolve);
-		db.connection.on('error', reject);
-	});
-};
-
-databaseDrop = function() {
-	// TODO: please backup database
-	//return new Promise(resolve, reject => {
-	//	db.dropDatabase(err => {
-	//		if (err) {
-	//			reject(err);
-	//			return;
-	//		}
-	//		resolve();
-	//	});
-	//});
-};
-
-modelsRequire = function() {
-	require('./db/users-schema');
-	require('./db/factor-sets-schema');
-
-	return Promise.all(
-		Object.keys(mongoose.models).map(model => {
-			return new Promise(resolve, reject => {
-				model.createIndex((err) => {
-					if (err) {
-						reject(err);
-						return;
-					}
-					resolve();
-				});
-			});
-		})
-	);
-};
-
-userCreate = function() {
-	let user = new Users({
-		login: 'Admin',
-		password: 'admin'
-	});
-
-	return user.save();
-};
-
-userTest = function() {
-	return Users.find({ login: 'Admin'});
-};
-
-factorSetCreate = function() {
-	// TODO: Cattell factor set JSON data
-};
