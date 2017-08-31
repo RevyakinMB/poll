6524d6ca5527114a7b7c1a3f72c519d19b39148c
Maxim Revyakin <mrevyakin@gmail.com>
2017-08-31 11:01:47 +0300

    home page testing list updated with progress data and results link

--- app/public/greet/greet.component.js
+++ app/public/greet/greet.component.js
@@ -1,23 +1,56 @@
 angular.module('greet')
 	.component('greet', {
-		controller: function greetController(Testing) {
+		controller: function greetController(Testing, authorizeService) {
 			this.testings = [];
 			Testing.query({
 				populate: true
 			},
 				function(ls) {
-					this.testings = ls.filter(function(t) {
-						if (t.attempts.length < t.idGroup.students.length) {
-							return true;
-						}
-						return !t.attempts.every(function(a) {
-							return a.finishedAt;
-						});
-					});
+					// TODO: filter out testing scheduled for any day except today
+					this.testings = ls;
+					// return;
+					// this.testings = ls.filter(function(t) {
+					//	if (t.attempts.length < t.idGroup.students.length) {
+					//		return true;
+					//	}
+					//	return !t.attempts.every(function(a) {
+					//		return a.finishedAt;
+					//	});
+					// });
 				}.bind(this),
 				function(err) {
 					console.log(err);
 				});
+
+			this.isPassed = function(t) {
+				console.log(t.idGroup.groupName, t.attempts.length,
+					t.attempts.length === t.idGroup.students.length,
+					t.idGroup.students.length,
+					t.attempts.every(function(a) {
+						return a.finishedAt;
+					}));
+
+				return t.attempts.length === t.idGroup.students.length &&
+					t.attempts.every(function(a) {
+						return a.finishedAt;
+					});
+			};
+
+			this.isInProgress = function(t) {
+				return t.attempts.length && !this.isPassed(t);
+			};
+
+			this.progress = function(t) {
+				return Math.floor(
+					(t.attempts.filter(function(a) {
+						return a.finishedAt;
+					}).length / t.idGroup.students.length) * 100
+				);
+			};
+
+			this.isLoggedIn = function() {
+				return authorizeService.isLoggedIn();
+			};
 		},
 		templateUrl: 'greet/greet.template.html'
 	});
